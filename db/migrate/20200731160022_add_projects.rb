a87bb8f64bf5e75f072cf5841ee27f5aa2c92bd2
William Baker <wbaker@gmail.com>
2020-07-31 12:05:09 -0400

    added projects model

---
+++ db/migrate/20200731160022_add_projects.rb
@@ -0,0 +1,9 @@
+class AddProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+end
