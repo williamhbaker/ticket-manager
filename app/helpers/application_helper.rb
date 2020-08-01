0009c433b5e3380491907e48d7bcd6376195ce7b
William Baker <wbaker@gmail.com>
2020-07-31 20:30:42 -0400

    cleaned up project view and access controls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def count_open_tickets(tickets)
     tickets.select { |t| t.status != 'fixed' }.size
   end
+
+  def format_status(status)
+    status.gsub('_', ' ').titleize
+  end
 end
